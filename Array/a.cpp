83a90ee60627b546dbd6994ff71f7c31dbdfb8e4
Ajeet Chaurasiya <cvajeetchaurasiya8887@gmail.com>
2026-02-12 14:20:19 +0530

    ..

---
+++ Array/a.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>v1(3,1);
+    vector<int> v2(4,2);
+    vector<int> v3(5,3);
+    vector<int>v4(6,4);
+    vector<int>v5(7,5);
+    vector<vector<int>>vv;
+
+    for(int i=0;i<vv.size();i++)
+    {
+        for(int j=0;j<vv[i].size();j++){
+            cout<<vv[i][j]<<" ";
+        
+        
+        }
+            cout<<"\n";
+        
+    }  
+} 
+
+
+
+
